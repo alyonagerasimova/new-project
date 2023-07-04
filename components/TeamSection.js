7a96fd80dab43650e52d116c3f8027b072b114bc
Alena Gerasimova <alena.gerasimova1999@gmail.com>
2023-07-04 23:33:19 +0400

    upd

--- components/TeamSection.js
+++ components/TeamSection.js
@@ -6,23 +6,36 @@ const TeamSection = () => {
         <div className="bg-white py-24 sm:py-32">
             <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
                 <div className="max-w-2xl">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Познакомтесь с нашим
-                        руководством</h2>
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Познакомтесь с нашей командой</h2>
                     {/*<p className="mt-6 text-lg leading-8 text-gray-600">Libero fames augue nisl porttitor nisi, quis. Id*/}
                     {/*    ac elit odio vitae elementum enim vitae ullamcorper suspendisse.</p>*/}
                 </div>
                 <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
                     <li>
-                        <div className="flex items-center gap-x-6">
+                        <div className="flex items-center gap-x-6" style={{display: "flex"}}>
                             <Image className="h-16 w-16 rounded-full"
                                    src="/avatar.jpg"
                                    alt=""
-                                   width={300}
-                                   height={300}/>
+                                   width={200}
+                                   height={200}/>
                             <div>
-                                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">Осипова
-                                    Татьяна Николаевна</h3>
+                                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                                    Татьяна</h3>
                                 <p className="text-sm font-semibold leading-6 text-indigo-600">ведущий специалист
+                                    департамента жилой недвижимости, специалист по ипотечному кредитованию
+                                    военнослужащих и сопровождению оформления целевых займов.</p>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-x-6" style={{display: "flex"}}>
+                            <Image className="h-16 w-16 rounded-full"
+                                   src="/avatar.jpg"
+                                   alt=""
+                                   width={200}
+                                   height={200}/>
+                            <div>
+                                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                                    Светлана</h3>
+                                <p className="text-sm font-semibold leading-6 text-indigo-600">специалист
                                     департамента жилой недвижимости, специалист по ипотечному кредитованию
                                     военнослужащих и сопровождению оформления целевых займов.</p>
                             </div>
